66b5e627ac4cae154cd370e8d19f53c2dfaa9f29
Randy Salazar <randysalazar@Randys-MacBook-Pro.local>
2016-11-23 17:50:24 -1000

    completed assignment:

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
+
+  var _key = 0;
+  var _value = 0;
   
+  // function setValue(key, value){
+  //    _key = key;
+  //    _value = value;
+  //   return getValue();
+  // }
+
+
+  function getValue(key){
+    if( _key){
+      return _value;
+    } else {
+      return null;
+    }
+  }
+
+    return{
+      setValue: setValue,
+      getValue: getValue
+    };
 };
